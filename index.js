73254ea182ac8b1ee921a6fdaa5eebd92477b56d
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-03-19 07:53:59 +0300

    Implement basic operations

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, World");
+add = (num1, num2) => num1 + num2;
+
+subtract = (num1, num2) => num1 - num2;
+
+multiply = (num1, num2) => num1 * num2;
+
+divide = (num1, num2) => {
+    if (num2 == 0) {
+        return NaN;
+    }
+    return num1 / num2;
+}
