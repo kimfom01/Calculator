a1cb9bea6457f0e6df19cbcdc5072a81261dbac8
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-03-19 08:03:58 +0300

    Enforce strict typing

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-add = (num1, num2) => num1 + num2;
+"use strict";
 
-subtract = (num1, num2) => num1 - num2;
+const add = (num1, num2) => num1 + num2;
 
-multiply = (num1, num2) => num1 * num2;
+const subtract = (num1, num2) => num1 - num2;
 
-divide = (num1, num2) => {
+const multiply = (num1, num2) => num1 * num2;
+
+const divide = (num1, num2) => {
     if (num2 == 0) {
         return NaN;
     }
     return num1 / num2;
 }
 
-operate = (num1, num2, operator) => {
+const operate = (num1, num2, operator) => {
     return operator(num1, num2);
 }
