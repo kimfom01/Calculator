709b6efbea67aa40ddf2ebcc1fa5767caf17f30e
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-03-31 00:08:36 +0300

    Implement basic calculator

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let num1;
+let num1 = "";
 let num2;
 let operator;
 
@@ -14,6 +14,7 @@ const divide = (num1, num2) => {
     if (num2 == 0) {
         return NaN;
     }
+
     return num1 / num2;
 }
 
@@ -21,14 +22,59 @@ const operate = (num1, num2, operator) => {
     return operator(num1, num2);
 }
 
-const buttons = document.querySelectorAll("button");
+const numbers = document.querySelectorAll(".numbers");
+const display = document.getElementById("display");
 
-buttons.forEach(btn => btn.addEventListener("click", (e) => {
+numbers.forEach(btn => btn.addEventListener("click", (e) => {
     e.preventDefault();
 
     const val = e.target.innerText;
 
-    const display = document.getElementById("display");
-
     display.innerText += val;
+
 }));
+
+const operations = document.querySelectorAll(".right");
+
+operations.forEach(btn => btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const operation = e.target.id;
+
+    switch (operation) {
+        case "addition":
+            num1 = parseFloat(display.innerText);
+            display.innerText = ""
+            operator = add;
+            break;
+        case "subtraction":
+            num1 = parseFloat(display.innerText);
+            display.innerText = ""
+            operator = subtract;
+            break;
+        case "multiplication":
+            num1 = parseFloat(display.innerText);
+            display.innerText = ""
+            operator = multiply;
+            break;
+        case "division":
+            num1 = parseFloat(display.innerText);
+            display.innerText = ""
+            operator = divide;
+            break;
+        case "equals":
+            num2 = parseFloat(display.innerText);
+            display.innerText = ""
+            display.innerText = operate(num1, num2, operator);
+            break;
+        default:
+            console.error("this shouldn't happen");
+    }
+}));
+
+
+const clear = document.getElementById("clear");
+
+clear.addEventListener("click", () => {
+    display.innerText = ""
+});
